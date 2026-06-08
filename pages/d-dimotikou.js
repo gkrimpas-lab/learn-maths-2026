4f7b345ced5767ac23dc166cafc808caec608641
gkrimpas-lab <gkrimpas@gmail.com>
2026-06-08 09:08:45 +0300

    Fix syntax error in d-dimotikou.js

--- pages/d-dimotikou.js
+++ pages/d-dimotikou.js
@@ -456,7 +456,7 @@ export default function DDimotikou() {
             </div>
 
           </div>
-        )}
+        )};
       </main>
 
       <footer className="bg-gray-800 text-gray-400 py-8 text-center text-sm mt-12">
