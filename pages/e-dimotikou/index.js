e67547853b49211976d8b9bd219340b147d2039f
gkrimpas-lab <gkrimpas@gmail.com>
2026-07-04 16:28:15 +0300

    Update index.js

--- pages/e-dimotikou/index.js
+++ pages/e-dimotikou/index.js
@@ -21,7 +21,8 @@ export default function EDimotikouMenu() {
     { id: '14-trigona-pleures', label: '🔺 14. Τρίγωνα - Πλευρές', href: '/e-dimotikou/14-trigona-pleures' },
     { id: '15-kathetes-eutheies', label: '🔺 15. Κάθετες - Ευθείες', href: '/e-dimotikou/15-kathetes-eutheies' },
     { id: '16-apostasi-simeiou-eutheias', label: '🔺 16. Απόσταση Σημείου από Ευθεία', href: '/e-dimotikou/16-apostasi-simeiou-eutheia' },
-    { id: '17-ipsos-trigonou', label: '🔺 17. Ύψος Τριγώνου', href: '/e-dimotikou/17-ipsos-trigonou' }
+    { id: '17-ipsos-trigonou', label: '🔺 17. Ύψος Τριγώνου', href: '/e-dimotikou/17-ipsos-trigonou' },
+    { id: '18-aksonas-simmetrias', label: '🔺 18. Άξονας συμμετρίας', href: '/e-dimotikou/18-aksonas-simmetrias' }
   ];
 
   return (
