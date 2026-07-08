b1d92ca756466ffefd0f62680fe93fd78ef1be2b
gkrimpas-lab <gkrimpas@gmail.com>
2026-07-08 15:21:13 +0300

    Update index.js

--- pages/st-dimotikou/index.js
+++ pages/st-dimotikou/index.js
@@ -5,7 +5,8 @@ import { LAYOUT } from '../../shared/layout-config';
 
 export default function STDimotikouMenu() {
   const modules = [
-    { id: '1-fisikoi-arithmoi', label: '🍕 1. Φυσικοί Αριθμοί', href: '/st-dimotikou/1-fisikoi-arithmoi' }
+    { id: '1-fisikoi-arithmoi', label: '🍕 1. Φυσικοί Αριθμοί', href: '/st-dimotikou/1-fisikoi-arithmoi' },
+    { id: '2-dekadikoi', label: '🍕 2. Δεκαδικοί Αριθμοί', href: '/st-dimotikou/2-dekadikoi' }
 
   ];
 
