6e0b90c51c1ab121e927d0e3272aa760304a11e7
gkrimpas-lab <gkrimpas@gmail.com>
2026-07-09 11:33:39 +0300

    Add new module for addition of natural numbers

--- pages/st-dimotikou/index.js
+++ pages/st-dimotikou/index.js
@@ -8,7 +8,8 @@ export default function STDimotikouMenu() {
     { id: '1-fisikoi-arithmoi', label: '🍕 1. Φυσικοί Αριθμοί', href: '/st-dimotikou/1-fisikoi-arithmoi' },
     { id: '2-dekadikoi', label: '🍕 2. Δεκαδικοί Αριθμοί', href: '/st-dimotikou/2-dekadikoi' },
     { id: '3-arithmoi-dekadika-klasmata', label: '🍕 3. Δεκαδικοί Αριθμοί σε Δεκαδικά Κλάσματα', href: '/st-dimotikou/3-arithmoi-dekadika-klasmata' },
-    { id: '4-sigkrisi-arithmon', label: '🍕 4. Σύγκριση Δεκαδικών Αριθμών', href: '/st-dimotikou/4-sigkrisi-arithmon' }
+    { id: '4-sigkrisi-arithmon', label: '🍕 4. Σύγκριση Δεκαδικών Αριθμών', href: '/st-dimotikou/4-sigkrisi-arithmon' },
+    { id: '5-prosthesi', label: '🍕 5. Πρόσθεση Φυσικών Αριθμών', href: '/st-dimotikou/5-prosthesi' }
   ];
 
   return (
