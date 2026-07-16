2608e0c14fd7121e2edd8a7c6f7a115daab1ad6e
gkrimpas-lab <gkrimpas@gmail.com>
2026-07-17 01:40:43 +0300

    Remove extra newline in index.js

--- pages/st-dimotikou/index.js
+++ pages/st-dimotikou/index.js
@@ -33,7 +33,6 @@ export default function STDimotikouMenu() {
     { id: '26-prosthesi-klasmaton', label: '🍕 26. Πρόσθεση Κλασμάτων', href: '/st-dimotikou/26-prosthesi-klasmaton' },
     { id: '27-afairesi-klasmaton', label: '🍕 27. Αφαίρεση Κλασμάτων', href: '/st-dimotikou/27-afairesi-klasmaton' },
     { id: '28-pollaplasiasmos-klasmaton', label: '🍕 28. Πολλαπλασιασμός Κλασμάτων', href: '/st-dimotikou/28-pollaplasiasmos-klasmaton' }
-    
   ];
 
   return (
