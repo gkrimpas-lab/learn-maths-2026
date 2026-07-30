f86c6dcf90a4662d0bdf453580dcde56453981d8
gkrimpas-lab <gkrimpas@gmail.com>
2026-07-30 14:46:38 +0300

    Update index.js

--- pages/d-dimotikou/index.js
+++ pages/d-dimotikou/index.js
@@ -10,7 +10,7 @@ export default function DDimotikouMenu() {
     { id: '3-prosthesi-afairesi', label: '🍕 3. Πρόσθεση - Αφαίρεση Αντίστροφες', href: '/d-dimotikou/3-prosthesi-afairesi' },
     { id: '4-pollaplasiasmos', label: '🍕 4. Πολλαπλασιασμός', href: '/d-dimotikou/4-pollaplasiasmos' },
     { id: '5-diairesi', label: '🍕 5. Διαίρεση με 1 ψηφίο', href: '/d-dimotikou/5-diairesi' },
-    { id: '6-dekadikoi', label: '✂️ 6. Δεκαδικοί', href: '/d-dimotikou/6-dekadikoi' },
+    { id: '7-dekadikoi_sinexeia', label: '✂️ 7. Δεκαδικά Κλάσματα και Δεκαδικοί', href: '/d-dimotikou/7-dekadikoi_sinexeia' },
     { id: '1-megaloi-arithmoi', label: '🍕 10. Μεγάλοι Αριθμοί', href: '/d-dimotikou/1-megaloi-arithmoi' },
     { id: '2-katheti-diairesi', label: '🔄 20. Κάθετη Διαίρεση', href: '/d-dimotikou/2-katheti-diairesi' },
     { id: '3-dekadikoi', label: '✂️ 30. Δεκαδικοί', href: '/d-dimotikou/3-dekadikoi' },
