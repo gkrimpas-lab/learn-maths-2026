1c37720fde88f719ee30c0e146ca3c14949cbcaa
gkrimpas-lab <gkrimpas@gmail.com>
2026-07-31 23:00:45 +0300

    Update index.js

--- pages/d-dimotikou/index.js
+++ pages/d-dimotikou/index.js
@@ -18,9 +18,8 @@ export default function DDimotikouMenu() {
     { id: '11-dekadikoi-3-psifia', label: '✂️ 11. Δεκαδικοί με 3 ψηφία', href: '/d-dimotikou/11-dekadikoi-3-psifia' },
     { id: '12-diairesi-deka-ekato-xilia', label: '🎯 12. Διαίρεση με 10, 100, 1000', href: '/d-dimotikou/12-diairesi-deka-ekato-xilia' },
     { id: '1-megaloi-arithmoi', label: '🍕 100. Μεγάλοι Αριθμοί', href: '/d-dimotikou/1-megaloi-arithmoi' },
-    { id: '2-katheti-diairesi', label: '🔄 200. Κάθετη Διαίρεση', href: '/d-dimotikou/2-katheti-diairesi' },
-    { id: '6-diairesi-deka-ekato-xilia', label: '🎯 300. Διαίρεση με 10, 100, 1000', href: '/d-dimotikou/6-diairesi-deka-ekato-xilia' }
-    
+    { id: '2-katheti-diairesi', label: '🔄 200. Κάθετη Διαίρεση', href: '/d-dimotikou/2-katheti-diairesi' }
+        
   ];
 
   return (
