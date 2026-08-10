fda9f3f8068b137471bc158b75c6bdef65024790
gkrimpas-lab <gkrimpas@gmail.com>
2026-08-10 19:20:05 +0300

    Update index.js

--- pages/d-dimotikou/index.js
+++ pages/d-dimotikou/index.js
@@ -22,6 +22,7 @@ export default function DDimotikouMenu() {
     { id: '15-embadon', label: '🎯 15. Εμβαδόν', href: '/d-dimotikou/15-embadon' },
     { id: '16-tetrapleura', label: '🎯 16. Τετράπλευρα', href: '/d-dimotikou/16-tetrapleura' },
     { id: '17-perimetros', label: '🎯 17. Περίμετρος', href: '/d-dimotikou/17-perimetros' },
+    { id: '18-simmetria', label: '🎯 18. Συμμετρία', href: '/d-dimotikou/18-simmetria' },
     { id: '1-megaloi-arithmoi', label: '🍕 100. Μεγάλοι Αριθμοί', href: '/d-dimotikou/1-megaloi-arithmoi' },
     { id: '2-katheti-diairesi', label: '🔄 200. Κάθετη Διαίρεση', href: '/d-dimotikou/2-katheti-diairesi' }
         
