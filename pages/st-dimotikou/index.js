a4c4ae34df55b2dc95f4664f042b2d495126885f
gkrimpas-lab <gkrimpas@gmail.com>
2026-08-16 23:17:27 +0300

    Fix ID for divisors entry in index.js

--- pages/st-dimotikou/index.js
+++ pages/st-dimotikou/index.js
@@ -17,7 +17,7 @@ export default function STDimotikouMenu() {
     { id: '10-proteraiotita-prakseon', label: '🍕 10. Προτεραιότητα Πράξεων', href: '/st-dimotikou/10-proteraiotita-prakseon' },
     { id: '11-problimata', label: '🍕 11. Προβλήματα', href: '/st-dimotikou/11-problimata' },
     { id: '12-stroggilopoiisi', label: '🍕 12. Στρογγυλοποίηση Αριθμών', href: '/st-dimotikou/12-stroggilopoiisi' },
-    { id: '12-diairetes', label: '🍕 13. Διαιρέτες Αριθμού', href: '/st-dimotikou/12-diairetes' },
+    { id: '13-diairetes', label: '🍕 13. Διαιρέτες Αριθμού', href: '/st-dimotikou/13-diairetes' },
     { id: '13-mkd', label: '🍕 14. Μέγιστος Κοινός Διαιρέτης (ΜΚΔ)', href: '/st-dimotikou/13-mkd' },
     { id: '14-kritiria-diairetotitas', label: '🍕 15. Κριτήρια Διαιρετότητας', href: '/st-dimotikou/14-kritiria-diairetotitas' },
     { id: '15-protoi', label: '🍕 16. Πρώτοι - Σύνθετοι Αριθμοί', href: '/st-dimotikou/15-protoi' },
