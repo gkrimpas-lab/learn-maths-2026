18030fc3d8bc0071ae91aee90b36c02f153741c5
gkrimpas-lab <gkrimpas@gmail.com>
2026-08-22 22:32:02 +0300

    Update index.js

--- pages/st-dimotikou/index.js
+++ pages/st-dimotikou/index.js
@@ -30,7 +30,7 @@ export default function STDimotikouMenu() {
     { id: '23-klasma', label: '🍕 23. Κλάσματα', href: '/st-dimotikou/23-klasma' },
     { id: '24-klasma-se-dekadiko', label: '🍕 24. Κλάσματα σε Δεκαδικό', href: '/st-dimotikou/24-klasma-se-dekadiko' },
     { id: '25-isodinama-klasmata', label: '🍕 25. Ισοδύναμα Κλάσματα', href: '/st-dimotikou/25-isodinama-klasmata' },
-    { id: '25-sigkrisi-klasmaton', label: '🍕 26. Σύγκριση Κλασμάτων', href: '/st-dimotikou/25-sigkrisi-klasmaton' },
+    { id: '26-sigkrisi-klasmaton', label: '🍕 26. Σύγκριση Κλασμάτων', href: '/st-dimotikou/26-sigkrisi-klasmaton' },
     { id: '26-prosthesi-klasmaton', label: '🍕 27. Πρόσθεση Κλασμάτων', href: '/st-dimotikou/26-prosthesi-klasmaton' },
     { id: '27-afairesi-klasmaton', label: '🍕 28. Αφαίρεση Κλασμάτων', href: '/st-dimotikou/27-afairesi-klasmaton' },
     { id: '28-pollaplasiasmos-klasmaton', label: '🍕 29. Πολλαπλασιασμός Κλασμάτων', href: '/st-dimotikou/28-pollaplasiasmos-klasmaton' },
