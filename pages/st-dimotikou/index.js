3275cd8bc9ebc63e2e9078ced3c2d068e7fabd15
gkrimpas-lab <gkrimpas@gmail.com>
2026-08-25 15:22:44 +0300

    Update index.js

--- pages/st-dimotikou/index.js
+++ pages/st-dimotikou/index.js
@@ -42,7 +42,8 @@ export default function STDimotikouMenu() {
     { id: '35-gnostos-meion-agnostos', label: '🍕 35. Εξίσωση α - x = β', href: '/st-dimotikou/35-gnostos-meion-agnostos' },
     { id: '36-gnostos-epi-agnostos', label: '🍕 36. Εξίσωση α * x = β ή x * α = β', href: '/st-dimotikou/36-gnostos-epi-agnostos' },
     { id: '37-agnostos-dia-gnostos', label: '🍕 37. Εξίσωση x : α = β', href: '/st-dimotikou/37-agnostos-dia-gnostos' },
-    { id: '38-gnostos-dia-agnostos', label: '🍕 38. Εξίσωση α : x = β', href: '/st-dimotikou/38-gnostos-dia-agnostos' }
+    { id: '38-gnostos-dia-agnostos', label: '🍕 38. Εξίσωση α : x = β', href: '/st-dimotikou/38-gnostos-dia-agnostos' },
+    { id: '39-epanalipsi-2', label: '🍕 39. Επαναληπτικές Ερωτήσεις Ενοτήτων 32 έως 38', href: '/st-dimotikou/39-epanalipsi-2' }
   ];
 
   return (
