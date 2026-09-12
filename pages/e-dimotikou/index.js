2d4ddcce5d021bbc60bade291c929d46362ba336
gkrimpas-lab <gkrimpas@gmail.com>
2026-09-12 16:47:32 +0300

    Fix ID for distance from line entry in index.js

--- pages/e-dimotikou/index.js
+++ pages/e-dimotikou/index.js
@@ -21,7 +21,7 @@ export default function EDimotikouMenu() {
     { id: '14-trigona', label: '🔺 14. Τρίγωνα - Γωνίες', href: '/e-dimotikou/14-trigona' },
     { id: '15-trigona-pleures', label: '🔺 15. Τρίγωνα - Πλευρές', href: '/e-dimotikou/15-trigona-pleures' },
     { id: '16-kathetes-eutheies', label: '🔺 16. Κάθετες - Ευθείες', href: '/e-dimotikou/16-kathetes-eutheies' },
-    { id: '16-apostasi-simeiou-eutheias', label: '🔺 17. Απόσταση Σημείου από Ευθεία', href: '/e-dimotikou/16-apostasi-simeiou-eutheia' },
+    { id: '17-apostasi-simeiou-eutheias', label: '🔺 17. Απόσταση Σημείου από Ευθεία', href: '/e-dimotikou/17-apostasi-simeiou-eutheia' },
     { id: '17-ipsos-trigonou', label: '🔺 18. Ύψος Τριγώνου', href: '/e-dimotikou/17-ipsos-trigonou' },
     { id: '18-aksonas-simmetrias', label: '🔺 19. Άξονας συμμετρίας', href: '/e-dimotikou/18-aksonas-simmetrias' },
     { id: '19-mikos-kiklou', label: '🔺 20. Μήκος Κύκλου', href: '/e-dimotikou/19-mikos-kiklou' },
