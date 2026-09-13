e6c20091ffa37c54b2fc75a86e7f3a2d366a5573
gkrimpas-lab <gkrimpas@gmail.com>
2026-09-13 14:42:06 +0300

    Fix ID for 'Εμβαδό Σχημάτων' entry in index.js

--- pages/e-dimotikou/index.js
+++ pages/e-dimotikou/index.js
@@ -30,7 +30,7 @@ export default function EDimotikouMenu() {
     { id: '23-kanonika-poligona', label: '🔺 23. Κανονικά Πολύγωνα', href: '/e-dimotikou/23-kanonika-poligona' },
     { id: '24-perimetros', label: '🔺 24. Περίμετρος', href: '/e-dimotikou/24-perimetros' },
     { id: '25-embado', label: '🔺 25. Εμβαδό', href: '/e-dimotikou/25-embado' },
-    { id: '25-embado-sximaton', label: '🔺 26. Εμβαδό Σχημάτων', href: '/e-dimotikou/25-embado-sximaton' },
+    { id: '26-embado-sximaton', label: '🔺 26. Εμβαδό Σχημάτων', href: '/e-dimotikou/26-embado-sximaton' },
     { id: '26-monades-epifaneias', label: '🔺 27. Μονάδες Μέτρησης Επιφάνειας', href: '/e-dimotikou/26-monades-epifaneias' },
     { id: '27-ennoia-ogkou', label: '🔺 28. Όγκος', href: '/e-dimotikou/27-ennoia-ogkou' },
     { id: '28-ogkoi-sximaton', label: '🔺 29. Όγκοι Σχημάτων', href: '/e-dimotikou/28-ogkoi-sximaton' }
