b57b3784937eec9e36d458776e0e2217afe0aa2c
gkrimpas-lab <gkrimpas@gmail.com>
2026-09-24 20:15:45 +0300

    Add new item for repetition section in index.js

--- pages/st-dimotikou/index.js
+++ pages/st-dimotikou/index.js
@@ -57,7 +57,8 @@ export default function STDimotikouMenu() {
     { id: '50-pososta', label: '📐 50. Ποσοστά', href: '/st-dimotikou/50-pososta' },
     { id: '51-brisko-pososta', label: '📐 51. Προβλήματα με Ποσοστά', href: '/st-dimotikou/51-brisko-pososta' },
     { id: '52-brisko-arxiki-timi', label: '📐 52. Εύρεση Αρχικής τιμής', href: '/st-dimotikou/52-brisko-arxiki-timi' },
-    { id: '53-ksero-arxiki-teliki-timi', label: '📐 53. Ξέρω αρχική και Τελική Τιμή. Εύρεση ποσοστού', href: '/st-dimotikou/53-ksero-arxiki-teliki-timi' }
+    { id: '53-ksero-arxiki-teliki-timi', label: '📐 53. Ξέρω αρχική και Τελική Τιμή. Εύρεση ποσοστού', href: '/st-dimotikou/53-ksero-arxiki-teliki-timi' },
+    { id: '54-epanalipsi-3', label: '🏆 54. Επανάληψη ( Κεφ. 40 - 53 )', href: '/st-dimotikou/54-epanalipsi-3' },
   ];
 
   return (
