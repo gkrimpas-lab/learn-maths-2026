2f2c4019bc39d38de78120f96a9c23e61c44e62e
gkrimpas-lab <gkrimpas@gmail.com>
2026-09-24 10:31:29 +0300

    Update index.js

--- pages/st-dimotikou/index.js
+++ pages/st-dimotikou/index.js
@@ -56,7 +56,8 @@ export default function STDimotikouMenu() {
     { id: '49-methodos-trion-ant-analoga', label: '📐 49. Μέθοδος των τριών - Αντιστρόφως ανάλογα ποσά', href: '/st-dimotikou/49-methodos-trion-ant-analoga' },
     { id: '50-pososta', label: '📐 50. Ποσοστά', href: '/st-dimotikou/50-pososta' },
     { id: '51-brisko-pososta', label: '📐 51. Προβλήματα με Ποσοστά', href: '/st-dimotikou/51-brisko-pososta' },
-    { id: '52-brisko-arxiki-timi', label: '📐 52. Εύρεση Αρχικής τιμής', href: '/st-dimotikou/52-brisko-arxiki-timi' }
+    { id: '52-brisko-arxiki-timi', label: '📐 52. Εύρεση Αρχικής τιμής', href: '/st-dimotikou/52-brisko-arxiki-timi' },
+    { id: '53-ksero-arxiki-teliki-timi', label: '📐 53. Ξέρω αρχική και Τελική Τιμή. Εύρεση ποσοστού', href: '/st-dimotikou/53-ksero-arxiki-teliki-timi' }
   ];
 
   return (
